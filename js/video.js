e5ab82a76cbe6d7747a25d41571473d348a6292a
mohammaduzzal <aalp72142@gmail.com>
2024-10-03 12:38:48 +0600

    added the complete main section with js

--- js/video.js
+++ js/video.js
@@ -1,3 +1,16 @@
+
+// convert time
+function timeString(time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
+
+
 // fetch,load,show categories on html
 
 // loadCategories function
@@ -43,10 +56,16 @@ const displayVideos = (videos) =>{
     const card = document.createElement('div')
     card.classList = "card card-compact"
     card.innerHTML =`
-     <figure class="h-[200px]">
+     <figure class="h-[200px] relative">
     <img
       src= ${video.thumbnail}
-      alt="Shoes" class="w-full h-full object-cover" />
+      alt="" class="w-full h-full object-cover" />
+      ${
+        video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black text-white p-1 rounded">
+      ${timeString(video.others.posted_date)}
+      </span>`
+      }
+    
   </figure>
   <div class="px-0 py-2 flex gap-2">
   <div>
@@ -56,7 +75,7 @@ const displayVideos = (videos) =>{
   <h2 class="font-bold">${video.title}</h2>
   <div class="flex items-center gap-2">
   <p class="text-gray-400">${video.authors[0].profile_name}</p>
-  <img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" />
+ ${video.authors[0].verified === true ? ` <img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" />` : "" }
   </div>
   
   <p></p>
